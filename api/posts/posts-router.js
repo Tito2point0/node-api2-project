cda9ea9aab02cd23017d21aba471a603b71899bc
sanjiro <defconx00@gmail.com>
2023-11-21 20:30:13 -0500

    delete working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -63,8 +63,25 @@ router.post('/', (req, res) => {
             })
     }
 })
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id',  async (req, res) => {
+    try {
+        const maybe = await Post.findById(req.params.id)
+        if (!maybe) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            // const deletePost =  
+            await Post.remove(req.params.id)
+            res.json(maybe)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack
+        })
+}
 })
 router.put('/:id', (req, res) => {
 
